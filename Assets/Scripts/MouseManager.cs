73c80b6a42a27fcdb4882176dc643656dc4abf98
Jonhathan <jonhathan.nerv@gmail.com>
2020-05-10 14:48:36 -0300

    Mouse Manager
    Mouse Manager

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,18 +1,86 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
+    public Vector2 panLimit;
+    public float minY = 40f;
+    public float maxY = 150f;
+    public float scrollSpeed = 10f;
+
+    public float panSpeed = 30f;
+
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+
+                pos = transform.position + dragStartPosition - dragCurrentPosition;
+
+            }
+        }
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+        transform.position = pos;
+
     }
+
 }
---
+++ Assets/Scripts/TargetName.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetName : MonoBehaviour
+{
+    //private new Camera camera;
+    public string targetsTag;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag(targetsTag))
+                {
+                    Debug.Log(hit.collider.gameObject.name);
+                }
+
+            }
+        }
+    }
+}
