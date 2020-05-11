1019b67d4c778eea0b5810e36792395573ba207c
5ruf <5ruf@informatik.uni-hamburg.de>
2020-05-11 15:10:09 +0200

    Finished task 1. Script needs boundaries (and movement speed of camera --> 0.1) from GameManager --> definePanning(...)

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /* 
- * camera rotation = (45, 0, 0) --> CoSy fits
- * 
- * 
- * tasks:  
- * While holding the right mouse button, moving the mouse pans the camera (on the XZ-plane)  x
- * The camera cannot be moved outside the boundaries of the terrain (x)
- * scrolling the mouse wheel zooms the camera in and out 
- * There are limits for the minimum and maximum zoom 
- * Left clicking a tile outputs the type of tile as text on the console (the name of the GameObject is sufficient)
- * 
- * 
+ * Mouse control to 
+ *      - move the camera by panning. 
+ *      - zoom in/ out by scrolling. 
+ *      - Click objects.  *      
+ *      
+ *      Camera has been rotated back to y=0° to make coding less confusing. --> CoSy fits to cam CoSy
  */
 public class MouseManager : MonoBehaviour
 {
@@ -21,82 +16,131 @@ public class MouseManager : MonoBehaviour
     [Tooltip("Just put main camera in 0_O")]
     public GameObject mainCamera;
 
-    // needs to fit so screensize
-    [Tooltip("~ sensitivity of mouse. 0.1f should be fine.")]
-    public float mainCameraMovementSpeed = 0.1f;
+    // needs to fit to screensize
+    // ~ sensitivity of mouse. 0.1f should be fine.
+    private float mainCameraMovementSpeed = 0.1f;
 
-    public float zoomingSpeed = 1;
+    // need to be set from GameManager: Boundaries of terrain: min x (left), max x (right), min z (near), max z(far).
+    // this are Demo data
+    private float boundaryMinX = -40f;
+    private float boundaryMaxX = 40f;
+    private float boundaryMinZ = 5 - 45;
+    private float boundaryMaxZ = 55 - 5;
 
-    // have to come from outside --> GameManager? 
-    [Tooltip("Boundaries of terrain: negative x (left), positive x (right), negative z (near), positive z(far)")]
-    public Vector4 boundariesOfTerrain = new Vector4 (-40, +40, -50, 30);
-   
+    // need to be set from GameManager:
+    // Boundaries of camera height: max: highest camera position (min zoom), min: lowest position (max zoom)
+    private float boundaryMinY = 2.5f;
+    private float boundaryMaxY = 75f;
+    private float zoomingSpeed = 10;
+
     // position of mouse at first registation of right click
-    private Vector3 lastMousePosition; 
+    private Vector3 lastMousePosition;
+    
+    // updated to calculated position of camera
+    private Vector3 CalculatedCamPos;
+
+    // to save calculated camera movements
+    private Vector2 panMovement; 
+    private float zoomYZMovement;
 
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        
-    }
-
+   
+    
     // Update is called once per frame
     void Update()
     {
-        zooming();
+        // CalculatedCamPos = mainCamera.transform.position;
 
-        // Camera.main.ScreenToWorldPoint(Input.mousePosition) 
-        if (Input.GetMouseButtonDown(1))
-        {   
-            lastMousePosition = Input.mousePosition;
-        }
+        // saves this as last mouse pposition if button is pressed in this frame
+        if (Input.GetMouseButtonDown(1)) lastMousePosition = Input.mousePosition;        
 
-        if (Input.GetMouseButton(1))
-        {
-            panMainCamera();
-        }
+        // returns name of clicked object in console
+        if (Input.GetMouseButtonDown(0)) Debug.Log("Hello, I am " + getClickedObject());
+        
+        SetNewMainCamPos();
+    }
 
-        // x, y, z = campostion.x, y,z ... 
-        // camPosition = (x from panning, y from zooming, z from panning
+    /* 
+     * Calculates and sets new positon of main camera
+     */
+    private void SetNewMainCamPos()
+    {
+        // get calculated cam movements
+        panMovement = Input.GetMouseButton(1) ? panning() : Vector2.zero;
+        zoomYZMovement = Input.mouseScrollDelta.y != 0 ? zooming() : 0f;
 
+        // calculate the theoretical camera position - not regarding boundaries/ max/ min zoom
+        // x is included in zooming to avoid "loosing focus". 
+        // Otherwise, the view screen would be moved while moving. (because of camera angle)
+        CalculatedCamPos = mainCamera.transform.position + new Vector3(
+            -panMovement.x,
+            -zoomYZMovement,
+            (zoomYZMovement - panMovement.y)
+            );
+
+        // Clamping the CamPos --> camera move within boundaries, has min/ max zoom. 
+        // Math.clamp() --> clamping --> camera moves only in boundaries of map. 
+        mainCamera.transform.position = new Vector3(
+            Mathf.Clamp(CalculatedCamPos.x, boundaryMinX, boundaryMaxX),
+            Mathf.Clamp(CalculatedCamPos.y, boundaryMinY, boundaryMaxY),
+            Mathf.Clamp(CalculatedCamPos.z, boundaryMinZ, boundaryMaxZ)
+            );
     }
 
 
     /*
-     * Method for moving the camera
-     * needs to be cleaned up
-     * T
-     */ 
-    private void panMainCamera(){
+     * calculates xz movement for the camera
+     */
+    private Vector2 panning(){
+       // mouse movement since last frame
+       // save current mouse position as last one for next frame
         Vector3 mouseDelta = Input.mousePosition - lastMousePosition;
-        //       mainCamera.transform.position += new Vector3(-mouseDelta.x * mainCameraMovementSpeed, 0, -mouseDelta.y * mainCameraMovementSpeed);
-        
-        //TODO: clean up, simplify
-        mainCamera.transform.position = 
-            new Vector3(
-                Mathf.Clamp((mainCamera.transform.position.x - (mouseDelta.x * mainCameraMovementSpeed)), boundariesOfTerrain.x, boundariesOfTerrain.y),
-                mainCamera.transform.position.y,
-                Mathf.Clamp((mainCamera.transform.position.z - (mouseDelta.y * mainCameraMovementSpeed)), boundariesOfTerrain.z, boundariesOfTerrain.w));
         lastMousePosition = Input.mousePosition;
+
+        // return calculated movement on x & z axis
+        return new Vector2(
+                mouseDelta.x * mainCameraMovementSpeed,
+                mouseDelta.y * mainCameraMovementSpeed);        
     }
 
 
+    /* 
+     * calculates zooming movement for camera positioning. 
+     */ 
     private float zooming()
     {
-        float zoomFactor = 0;
-        // y: positive --> up
-        // Vector2 wheeled = Input.mouseScrollDelta;
-
-        zoomFactor += Input.mouseScrollDelta.y * zoomingSpeed;
-
-
-        return zoomFactor;
+        return Input.mouseScrollDelta.y * zoomingSpeed * Time.deltaTime;
     }
 
 
+    /* should be called by GameManager to set xz boundaries
+     * Manupulations to z values are necessary because of the camera angle. 
+     */ 
+    public void definePanning(float xMin, float xMax, float zMin, float zMax, float movementSpeed)
+    {        
+        (boundaryMinX, boundaryMaxX, boundaryMinZ, boundaryMaxZ, mainCameraMovementSpeed) = (xMin, xMax, zMin - 45, zMax - 5, movementSpeed);
+    }
 
-    
+
+ /* should be called by GameManager to set xz boundaries
+ * Set minimal and maximal height of camera. 
+ */
+    public void defineZoom(float yMin, float yMax, float speed)
+    {
+        (boundaryMinY, boundaryMaxY, zoomingSpeed) = (yMin, yMax, speed);
+    }
+
+    /*
+     * returns name of clicked object or error text if that's not possible. 
+     */ 
+    private string getClickedObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100)) return hit.transform.gameObject.name;
+        
+        return "unknown und probably too far away or too small to be clicked. Sorry :/";
+    }
 }
