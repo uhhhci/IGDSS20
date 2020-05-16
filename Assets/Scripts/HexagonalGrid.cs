8a0c46a531a0731a6e5189ff57060b58473874c9
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-05-16 22:59:44 +0200

    Change Heaxagonalgrid to receive dimensions through constructor -including height scaling
    HexagonalGrid now requests a Vector2 in its constructor. For hexagons
    its only neccessary to now either the height or width as the other one
    can be computed. However, instead of figuring out which is passed,
    HexagonalGrid now just requests both
    
    HexagonalGrid now requests a Vector2 in its constructor. For hexagons
    its only neccessary to now either the height or width as the other one
    can be computed. However, instead of figuring out which is passed,
    HexagonalGrid now just requests both.

--- Assets/Scripts/HexagonalGrid.cs
+++ Assets/Scripts/HexagonalGrid.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using UnityEngine;
 
-internal class HexagonalGrid
+internal class HexagonalGrid : Map
 {
     public Vector3 Origin { get; }
 
-    private int width;
-    private int height;
+    private int gridWidth;
+    private int gridHeight;
     private HexComponent[,] grid;
-    //Hardcoded as the orientation and scale in unity 
-    //Need to assume that all the tiles are equally sized
-    private float CELL_HEIGHT = 10;
-    private float HeightScale = 5;
+
+    private float cellHeight;
+    private float cellWidth;
 
 
-    public HexagonalGrid(int width, int height, Vector3 origin = new Vector3())
+    public HexagonalGrid(int width, int height, Vector2 cellDimensions, Vector3 origin = new Vector3())
     {
-        this.width = width;
-        this.height = height;
+        gridWidth = width;
+        gridHeight = height;
         Origin = origin;
         grid = new HexComponent[width, height];
+
+        //Use these, under the assumption that all tiles are equally sized
+        cellWidth = cellDimensions[0];
+        cellHeight = cellDimensions[1];
+
     }
 
     internal void AddCell(GameObject tile, int x, int y)
@@ -33,24 +37,20 @@ internal class HexagonalGrid
         grid[x, y].Height = height;
     }
 
-    internal void DrawGrid()
+    public override void Show()
     {
         var parent = new GameObject("MapGrid");
         parent.transform.position = Origin;
 
-        //Calculate once as we assume all tiles gameobjects are equally sized
-        var cellHeight = CELL_HEIGHT;
-        var cellWidth = (cellHeight / Mathf.Sqrt(3)) * 2;
-
-        for (int x = 0; x < width; x++)
+        for (int x = 0; x < gridWidth; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < gridHeight; y++)
             {
                 HexComponent gridComponent = grid[x, y];
                 bool insettedRow = x % 2 == 0;
                 var inset = Convert.ToInt32(insettedRow) * (cellHeight * 0.5f);
 
-                var worldPosition = new Vector3(x * cellWidth * 0.75f, gridComponent.Height * HeightScale, y * cellHeight + inset);
+                var worldPosition = new Vector3(x * cellWidth * 0.75f, gridComponent.Height, y * cellHeight + inset);
                 var obj = UnityEngine.Object.Instantiate(gridComponent.RenderObject, worldPosition, Quaternion.identity);
                 obj.name = $"{x}:{y}";
 
