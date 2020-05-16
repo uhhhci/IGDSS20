f8190e8a6bd0ac70746fe517d628f83fc28f4dad
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-05-16 22:51:34 +0200

    Move map functionality into map manager
    Previously the game manager was responsible for the logic behind grid
    assembly and texture reading. This is now moved into a map manager and a
    map superclass. This also allows for different map generation methods
    later on.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,33 +8,17 @@ public class GameManager : MonoBehaviour
     [SerializeField] private CameraManager cameraManager;
     [SerializeField] private MouseManager mouseManager;
     [SerializeField] private Texture2D _heightmap;
+    [SerializeField] private MapManager _mapManager;
     [SerializeField] private TileSet _tileSet;
-    
+
 
     void Start()
     {
         _ = _heightmap ?? throw new ArgumentNullException("Heightmap for map generation has not been set! Set one in the GameManager script");
 
-        var width = _heightmap.width;
-        var height = _heightmap.height;
-        var grid = new HexagonalGrid(width,height);
-
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                var color = _heightmap.GetPixel(x, y);
-                var grayValue = color.grayscale;
-                GameObject tile = GetTileFromColor(color);
-
-                grid.AddCell(tile, x, y);
-                grid.SetHeightOfCell(grayValue, x, y);
-            }
-        }
-
-
         cameraManager.cameraBoundaries = new Boundaries(lowerBounds, upperBounds);
-
-        grid.DrawGrid();
+        
+        var base_map = _mapManager.FromHeightmap(_heightmap, _tileSet, 5);
+        _mapManager.DrawMap(base_map);
     }
 }
---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public abstract class Map
+{
+    public abstract void Show();
+
+    public abstract void GetBoundaries();
+}
