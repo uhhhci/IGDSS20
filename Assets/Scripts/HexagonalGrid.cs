8bf3e2a8640bfe3522b4feb194a3cdb273d01c47
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-05-17 02:19:51 +0200

    Move worldPosition Calculation to AddCell() instead of Show()

--- Assets/Scripts/HexagonalGrid.cs
+++ Assets/Scripts/HexagonalGrid.cs
@@ -33,13 +33,15 @@ internal class HexagonalGrid : Map
 
     internal void AddCell(GameObject tile, int x, int y)
     {
-        var gridComponent = new HexComponent { RenderObject = tile };
+        var worldPos = CalculateWorldPosition(x, y);
+
+        var gridComponent = new HexComponent { RenderObject = tile, WorldPosition = worldPos };
         grid[x, y] = gridComponent;
     }
 
     internal void SetHeightOfCell(float height, int x, int y)
     {
-        grid[x, y].Height = height;
+        grid[x, y].SetHeight(height);
     }
 
     public override void Show()
@@ -52,25 +54,31 @@ internal class HexagonalGrid : Map
             for (int y = 0; y < gridHeight; y++)
             {
                 HexComponent gridComponent = grid[x, y];
-                bool insettedRow = x % 2 == 0;
-                var inset = Convert.ToInt32(insettedRow) * (cellHeight * 0.5f);
 
-                float worldPosX = x * cellWidth * 0.75f;
-                float worldPosZ = y * cellHeight + inset;
-                float worldPosY = gridComponent.Height;
+                var worldPos = gridComponent.WorldPosition;
 
-                maxWorldPosOnXAxis = worldPosX > maxWorldPosOnXAxis ? worldPosX : maxWorldPosOnXAxis;
-                maxWorldPosOnYAxis = worldPosY > maxWorldPosOnYAxis ? worldPosY : maxWorldPosOnYAxis;
-                maxWorldPosOnZAxis = worldPosZ > maxWorldPosOnZAxis ? worldPosZ : maxWorldPosOnZAxis;
-
-
-                var worldPosition = new Vector3(worldPosX, worldPosY, worldPosZ);
-                var obj = UnityEngine.Object.Instantiate(gridComponent.RenderObject, worldPosition, Quaternion.identity);
+                var obj = UnityEngine.Object.Instantiate(gridComponent.RenderObject, worldPos, Quaternion.identity);
                 obj.name = $"{x}:{y}";
 
-                obj.transform.SetParent(parent.transform);
+                obj.transform.SetParent(parent.transform, false);
             }
         }
+    }
+
+    private Vector3 CalculateWorldPosition(int x, int y)
+    {
+        bool insettedRow = x % 2 == 0;
+        var inset = Convert.ToInt32(insettedRow) * (cellHeight * 0.5f);
+
+        var worldPosX = x * cellWidth * 0.75f;
+        var worldPosY = 0;
+        var worldPosZ = y * cellHeight + inset;
+
+        maxWorldPosOnXAxis = worldPosX > maxWorldPosOnXAxis ? worldPosX : maxWorldPosOnXAxis;
+        maxWorldPosOnYAxis = worldPosY > maxWorldPosOnYAxis ? worldPosY : maxWorldPosOnYAxis;
+        maxWorldPosOnZAxis = worldPosZ > maxWorldPosOnZAxis ? worldPosZ : maxWorldPosOnZAxis;
+
+        return new Vector3(worldPosX, worldPosY, worldPosZ);
     }
 
     public override Boundaries GetBoundaries()
@@ -86,6 +94,11 @@ internal class HexagonalGrid : Map
     private class HexComponent
     {
         public GameObject RenderObject { get; set; }
-        public float Height { get; set; }
+        public Vector3 WorldPosition{ get; set; }
+
+        internal void SetHeight(float height)
+        {
+            WorldPosition = new Vector3(WorldPosition.x, height, WorldPosition.z);
+        }
     }
 }
