d34dc4974d0de8bc4ca813c6d0e8590dae209815
JannFreiwald <jann.freiwald@googlemail.com>
2020-05-18 10:56:28 +0200

    Added solution for assignment 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public GameObject[] _tilePrefabs; //References to the tile prefabs
+
+    public Texture2D _heightMap; //Reference to the height map texture file
+
+    private float _heightFactor = 50; //Multiplier for placement of tiles on the Y-axis
+
+    public Transform _tileHolder; //Reference to the parent object in the hierarchy for all spawned tiles
+
+    public MouseManager _mouseManager; //Reference to the MouseManager.Instance
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateMap();
+        _mouseManager.InitializeBounds(0, _heightMap.width * 10, 0, _heightMap.height * 8.66f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void GenerateMap()
+    {
+        for (int h = 0; h < _heightMap.height; h++)
+        {
+            for (int w = 0; w < _heightMap.width; w++)
+            {
+
+                Color c = _heightMap.GetPixel(w, h);
+                float max = c.maxColorComponent;
+                float tileHeight = _heightFactor * max;
+
+                //Determine tile type
+                GameObject selectedPrefab;
+
+                if (max == 0.0f)
+                {
+                    selectedPrefab = _tilePrefabs[0];
+                }
+                else if (max < 0.2f)
+                {
+                    selectedPrefab = _tilePrefabs[1];
+                }
+                else if (max < 0.4f)
+                {
+                    selectedPrefab = _tilePrefabs[2];
+                }
+                else if (max < 0.6f)
+                {
+                    selectedPrefab = _tilePrefabs[3];
+                }
+                else if (max < 0.8f)
+                {
+                    selectedPrefab = _tilePrefabs[4];
+                }
+                else
+                {
+                    selectedPrefab = _tilePrefabs[5];
+                }
+
+                GameObject go = Instantiate(selectedPrefab, _tileHolder);
+                go.transform.position = new Vector3(h * 8.66f, tileHeight, w * 10f + (h % 2 == 0 ? 0f : 5f));
+            }
+        }
     }
 }
--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -4,15 +4,100 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
+
+    public Camera _camera; //The camera object
+
+    private Vector3 _lastPanPosition; //The position of the mouse when the dragging begins
+
+    private float PanSpeed = 200f; //Movement speed multiplier of the camera translation
+    private float ZoomSpeedMouse = 50f; //Multiplier for zoom factor
+
+    public float[] BoundsX = new float[2]; //Camera bounds on the X axis
+    public float[] BoundsZ = new float[2]; //Camera bounds on the ZX axis
+    public float[] ZoomBounds = new float[] { 10f, 85f }; //Zoom bounds on the Y axis
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MouseSelection();
+        MousePanning();
+    }
+
+    void MouseSelection()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            int layerMask = LayerMask.GetMask("Tiles");
+
+            if (Physics.Raycast(ray, out hit, 1000f, layerMask))
+            {
+                Debug.Log("You selected the " + hit.collider.name);
+            }
+        }
+    }
+
+    void MousePanning()
+    {
+        // On mouse down, capture it's position.
+        // Otherwise, if the mouse is still down, pan the camera.
+        if (Input.GetMouseButtonDown(1))
+        {
+            _lastPanPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            PanCamera(Input.mousePosition);
+        }
+
+        // Check for scrolling to zoom the camera
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        ZoomCamera(scroll, ZoomSpeedMouse);
+    }
+
+    void PanCamera(Vector3 newPanPosition)
+    {
+        // Determine how much to move the camera
+        Vector3 offset = _camera.ScreenToViewportPoint(_lastPanPosition - newPanPosition);
+        Vector3 move = new Vector3(-offset.y * PanSpeed, 0, offset.x * PanSpeed);
+
+        // Perform the movement
+        _camera.transform.Translate(move, Space.World);
+
+        // Ensure the camera remains within bounds.
+        Vector3 pos = _camera.transform.position;
+        pos.x = Mathf.Clamp(_camera.transform.position.x, BoundsX[0], BoundsX[1]);
+        pos.z = Mathf.Clamp(_camera.transform.position.z, BoundsZ[0], BoundsZ[1]);
+        _camera.transform.position = pos;
+
+        // Cache the position
+        _lastPanPosition = newPanPosition;
+    }
+
+    void ZoomCamera(float offset, float speed)
+    {
+        if (offset == 0)
+        {
+            return;
+        }
+
+        _camera.fieldOfView = Mathf.Clamp(_camera.fieldOfView - (offset * speed), ZoomBounds[0], ZoomBounds[1]);
+    }
+
+    public void InitializeBounds(float Xleft, float Xright, float Ztop, float Zbottom)
+    {
+
+        BoundsX = new float[2] { Xleft, Xright };
+        BoundsZ = new float[2] { Ztop, Zbottom };
+
+        _camera.transform.position = new Vector3((Xleft + Xright) / 2, _camera.transform.position.y, (Ztop + Zbottom) / 2);
     }
 }
