53f97463ac86c2c3c1f0cd0db92ff5c707889b45
5ruf <5ruf@informatik.uni-hamburg.de>
2020-06-02 20:57:28 +0200

    Finshed and commented building.cs
    Still missing: TODO: after merge:
    - adapt comparison with neighboring fields and
    - make public GameObject tile a Tile.

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -5,47 +5,94 @@ using System;
 
 public class Building : MonoBehaviour
 {
-    private TileSet _tileSet; 
-    // public enum TileTypeEnum { WaterTile, SandTile, GrassTile, ForestTile, StoneTile, MountainTile  };
-    // public enum BuildingTypeEnum { Fishery, Lumberjack, Sawmill, SheepFarm, FrameworkKnitters, PotatoFarm, SchnappsDistillery };
-
-    //public enum ResourcesTypeEnum { Fish, Wood, Plank, Wool, Cloth, Potato, Schnapps};
-
+    [Tooltip("Type of Building")]
     public BuildingType buildingType;
+    [Tooltip("Money cost per economy cycle")]
     public int upkeep = 0;
+    [Tooltip("Costs in money/ planks to build building.")]
     public int buildCost_Money, buildCost_Plank;
 
+    // After merge, type should be Tile
+    [Tooltip("The tile where building is built on. ")]
     public GameObject tile;
+    [Tooltip("Type of neighboring tile that boosts efficiency of production. ")]
     public TileType efficientNeighboringTile;
-    public List<TileSet> goodNeighbor;
 
-    // has to be limited to 0-6
+    [Tooltip("min/ max count of efficiency pushing neighbars that are used for calculations.")]
+    [Range (0,6)]
     public int minEfficientNeigbor, maxEfficientNeighbor;
-    public RangeInt minAndMax;
 
-    // TODO this list hast to contain 0-2 items --> limit size
+    // this list hast to contain 0-2 items --> limit size
+    [Tooltip("0 - 2 resource types that are production input")]
     public List<ResourcesType> inputResources;
-    public ResourcesType outputResources;
+    [Tooltip("That's what the building produces.")]
+    public ResourcesType outputResource;
+    [Tooltip("How long is a production cycle in sec.")]
     public float resourceGenerationInterval;
+    [Tooltip("How many resources are produced in a cycle. ")]
     public int outputCount;
 
 
-
+    private float generationState;
+    private float effectiveGenerationTime;
+    private bool productionRunning;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //effectiveGenerationTime = (float) (2 - Efficiency()) * resourceGenerationInterval;
+        effectiveGenerationTime = (2 - 0.5f) * resourceGenerationInterval;
     }
 
     // Update is called once per frame
     void Update()
+    {      
+
+        if (productionRunning)
+        {
+            // working
+            generationState += Time.deltaTime;
+
+            // finished cycle
+            if (generationState >= effectiveGenerationTime)
+            {
+                // create resource and reset cycle
+                GameManager.winResource(outputResource, outputCount);
+                generationState -= effectiveGenerationTime;    
+                productionRunning = false;
+
+                // restart if possible
+                tryToStartProduction();
+            }
+        }
+        else
+        {
+            // try to start production            
+            tryToStartProduction();
+        }
+    }
+
+    /* 
+     * checks if production can be started. 
+     * If start is possible, the input res are taken from warehouse. 
+     */ 
+    private void tryToStartProduction()
     {
-        // TODO manage production cycle
-        // use efficiency
-        // GameManager.removeResource(type, 1)
-        // GameMananger.WinResource(type, int count)
+        if (CanProductionStart())
+        {
+            // start production
+            productionRunning = true;
+            inputResources.ForEach(delegate (ResourcesType it)
+            {
+                GameManager.removeResource(it, 1);
+            });
+        }
+        else
+        {
+            // Production start failed aka GameManager said no. 
+            productionRunning = false;
+        }
     }
 
     /* 
@@ -57,14 +104,24 @@ public class Building : MonoBehaviour
         // save neighbors. 
         List<GameObject> neighbors = new List<GameObject>();
         
+
+
         // neighbors = tile.getNeighboringTiles();
-        // TODO: or however you saved the neighbors.
+        // TODO: or however you saved the neighbors. + adapt next if
+        // 
+
+
+
 
         // count 'efficient' neighbors
         int counter = 0;
         float efficiency;
         neighbors.ForEach(delegate (GameObject it)
         {
+
+            // TODO
+
+            // check if item equals the efficient neighbor
             if (it.name == efficientNeighboringTile.ToString()) counter++;
         });
 
@@ -76,8 +133,20 @@ public class Building : MonoBehaviour
         return Mathf.Clamp(efficiency, 0, 1);
     }
 
-
-    
+    /* asks GameManager if input resources are available --> production is startable
+     */ 
+    private bool CanProductionStart()
+    {
+        bool val = true; 
+        inputResources.ForEach(delegate (ResourcesType it)
+        {
+            if (!GameManager.checkAvailability(it, 1))
+            {
+                val = false;
+            }
+        });
+        return val;
+    }
 
 
 
